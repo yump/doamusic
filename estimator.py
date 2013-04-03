0545803aeeac63f33228f9b669bd4f01de92d604
Russell Haley <rhaley@lily>
2013-04-03 09:07:13 -0500

    Some initial work on python MUSIC.

--- estimator.py
+++ estimator.py
@@ -1,7 +1,19 @@
+import scipy as sp
+from scipy import misc
+
 class estimator
 	
 	"""A class to estimate direction of arrival (DOA) using the Multiple
 	SIgnal Classification algorithm."""
 
-	def __init__(self):
-		pass
+	def __init__(self, antennas):
+		assert antennas.shape[1] == 3
+		assert antennas.dtype == 'float64'
+		self.antennas = antennas
+		self.numel = antennas.shape[0]
+		self.numsamples = 0
+
+	def newsamples(self,samples):
+		assert 
+		
+			
