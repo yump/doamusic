cb60e2d39c6c351cebb899b5b5736745a4679898
Russell Haley <rhaley@tamu.edu>
2013-04-05 02:11:23 -0500

    Further work on porting MUSIC to python 2.  Not yet functional.

--- estimator.py
+++ estimator.py
@@ -2,18 +2,23 @@ import scipy as sp
 from scipy import misc
 
 class estimator
-	
-	"""A class to estimate direction of arrival (DOA) using the Multiple
-	SIgnal Classification algorithm."""
-
+	"""
+	A class to estimate direction of arrival (DOA) using the Multiple
+	SIgnal Classification algorithm.
+	"""
 	def __init__(self, antennas):
 		assert antennas.shape[1] == 3
 		assert antennas.dtype == 'float64'
 		self.antennas = antennas
 		self.numel = antennas.shape[0]
 		self.numsamples = 0
+		self.covar = sp.matrix(sp.zeros((numel,numel)))
 
 	def newsamples(self,samples):
-		assert 
+		"""
+		Insert a N x num_antennas matrix of samples.
+		Each row is an iteration.
+		"""
+		assert samples.shape[1] == 3
 		
 			
