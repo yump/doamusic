f1be3363e05aa3c3d89a1e54a2911fdc14cd3750
Russell Haley <rhaley@tamu.edu>
2013-04-13 13:08:50 -0500

    We GPLv3 now.

--- util.py
+++ util.py
@@ -1,3 +1,21 @@
+#   Copyright 2013 Russell Haley
+#   (Please add yourself if you make changes)
+#
+#   This file is part of doamusic.
+#
+#   doamusic is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   doamusic is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with doamusic.  If not, see <http://www.gnu.org/licenses/>.
+
 import numpy as np
 import scipy as sp
 import math
